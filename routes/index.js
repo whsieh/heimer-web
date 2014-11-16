b3210b0e0c9b7efa4df8385b77dcf8791eec5d90
Kevin Wu <kevinwu.business@gmail.com>
2014-11-15 16:21:27 -0800

    Forgot to comit index.js.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+var path = require("path");
+var fs = require("fs");
+
+var order = JSON.parse(fs.readFileSync(path.join(__dirname, "../", "content", 'order.json')));
+var sections = [];
+
+order.files.forEach(function(file) {
+    var name = file.name;
+    var filename = file.filename;
+    var id = file.id;
+    var content = fs.readFileSync(path.join(__dirname, "../", "content", filename), "utf8");
+
+    sections.push({ id: id, name: name, content: content });
+});
+
+// sections.forEach(function(section){
+//     console.log(section.id);
+//     console.log(section.content);
+// });
+
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res) {
+  res.render("index", { title: "InstaParser", sections: sections });
 });
 
 module.exports = router;
