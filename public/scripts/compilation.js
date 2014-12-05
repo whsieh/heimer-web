e580a80fc0ea1792970be923492f2bbf23c094bd
Wenson Hsieh <whsieh@berkeley.edu>
2014-12-04 22:32:09 -0800

    Implemented output download functionality.

--- public/scripts/compilation.js
+++ public/scripts/compilation.js
@@ -3,6 +3,8 @@ $(function() {
     var editor = window.exports.editor
     var gencode = window.exports.gencode
 
+    var output = {}
+
     // Helper to convert language to CodeMirror language types
     var convertLanguageName = function(name) {
         if (name == "java") {
@@ -29,23 +31,38 @@ $(function() {
 
             var editorLanguage = convertLanguageName(language);
 
-            editor.selectOutput(editor.addOutput(editorLanguage, result.main.name, decodeURI(result.main.content)));
-            editor.addOutput(editorLanguage, result.util.name, decodeURI(result.util.content));
+            var mainFileContents = decodeURI(result.main.content)
+            var utilFileContents = decodeURI(result.util.content)
+            editor.selectOutput(editor.addOutput(editorLanguage, result.main.name, mainFileContents));
+            editor.addOutput(editorLanguage, result.util.name, utilFileContents);
+            output = {}
+            output[result.main.name] = mainFileContents;
+            output[result.util.name] = utilFileContents;
+
             if (result.classes.length != 0) {
                 var filenames = [];
                 var contents = [];
                 _.each(result.classes, function(classfile) {
                     filenames.push(classfile.name);
-                    contents.push(decodeURI(classfile.content));
+                    var classFileContents = decodeURI(classfile.content);
+                    contents.push(classFileContents);
+                    output[classfile.name] = classFileContents;
                 });
-
-                console.log(filenames, contents);
-
                 editor.addOutputDropdown(editorLanguage, "Classes", filenames, contents);
             } else {
-                editor.addOutput(editorLanguage, result.data.name, decodeURI(result.data.content));
+                var fileContents = decodeURI(result.data.content);
+                editor.addOutput(editorLanguage, result.data.name, dataFileContents);
+                output[result.data.name] = dataFileContents;
             }
         });
     });
+    $("#downloadOutput").click(function() {
+        var zip = new JSZip();
+        var root = zip.folder("src");
+        for (var filename in output)
+            root.file(filename, output[filename]);
 
+        var content = zip.generate( {type: "blob"} );
+        saveToFile(content, "parser.zip");
+    });
 });
